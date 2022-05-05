8f5133088e32d24a81adf8098962957309f7cd48
Saurabh Prakash <saurabhp75@gmail.com>
2022-05-06 01:00:29 +0530

    Add login code

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,6 +4,7 @@ import { json } from "@remix-run/node";
 
 import stylesUrl from "../styles/login.css";
 import { db } from "~/utils/db.server";
+import { createUserSession, login } from "~/utils/session.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
@@ -74,11 +75,20 @@ export const action: ActionFunction = async ({ request }) => {
     case "login": {
       // login to get the user
       // if there's no user, return the fields and a formError
+      const user = await login({ username, password });
+      console.log({ user });
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Username/Password combination is incorrect`,
+        });
+      }
       // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+      return createUserSession(user.id, redirectTo);
+      // return badRequest({
+      //   fields,
+      //   formError: "Not implemented",
+      // });
     }
     case "register": {
       const userExists = await db.user.findFirst({
